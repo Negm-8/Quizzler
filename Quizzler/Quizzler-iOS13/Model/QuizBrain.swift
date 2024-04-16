d3d2d2656fd46e5a9bf52fe0b20dec0bd7102d09
abdallah negm <113768788+Negm-8@users.noreply.github.com>
2024-04-16 14:58:26 +0200

    Add files via upload

---
+++ Quizzler/Quizzler-iOS13/Model/QuizBrain.swift
@@ -0,0 +1,54 @@
+
+import Foundation
+
+struct QuizBrain {
+    
+    var questionNumber = 0
+    var score = 0
+    
+    let quiz = [
+        Question(q: "هلا ياحبي.", a: "True"),
+        Question(q: "مفيش معاك ٣ مليون اخضر ابدا فيهم حياتي .", a: "True"),
+        Question(q: " هو لو معايا هاعرفك.", a: "True"),
+        Question(q: "خلاص ياعم الكحيان.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "ليه التعليم بيض.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+    ]
+    
+    func getQuestionText() -> String {
+        return quiz[questionNumber].text
+    }
+    
+    func getProgress() -> Float {
+        return Float(questionNumber) / Float(quiz.count)
+    }
+    
+    mutating func getScore() -> Int {
+        return score
+    }
+    
+     mutating func nextQuestion() {
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+    }
+    
+    mutating func checkAnswer(userAnswer: String) -> Bool {
+        if userAnswer == quiz[questionNumber].answer {
+            score += 1
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
